7f1924c9135e787fffa570414c590ca8789d4b4a
joostprins <joost_prins@hotmail.com>
2017-12-01 19:10:26 +0100

    Add more advanced color detection

--- imageRecognizer.py
+++ imageRecognizer.py
@@ -1,20 +1,34 @@
-# import the necessary packages
 import numpy as np
 import cv2
 import sys
+
+import time
+
+hsv = False
+greenLower = (29, 86, 6)
+greenUpper = (64, 255, 255)
 
 image1 = cv2.imread('im1.jpg', 1)
 image2 = cv2.imread('im2.jpg', 1)
 image3 = cv2.imread('im3.jpg', 1)
 image4 = cv2.imread('im4.jpg', 1)
 image = image1
+mask = None
 
-lower = np.array([100, 60, 0], dtype="uint8")
-upper = np.array([200, 180, 50], dtype="uint8")
+if hsv:
+    new_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(new_image, greenLower, greenUpper)
+else:
+    lower = np.array([100, 60, 0], dtype="uint8")
+    upper = np.array([200, 180, 50], dtype="uint8")
+    mask = cv2.inRange(image, lower, upper)
 
-# find the colors within the specified boundaries and apply
-# the mask
-mask = cv2.inRange(image, lower, upper)
+mask = cv2.erode(mask, None, iterations=2)
+mask = cv2.dilate(mask, None, iterations=2)
+# contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[0]
+
+# cv2.drawContours(image, contours, -1, (255,0,0),10)
+# cv2.imwrite("output.jpg", image)
 
 left_high = [sys.maxsize, sys.maxsize]
 
@@ -34,5 +48,4 @@ for p in locations:
     elif y >= right_low[1]:
         right_low[1] = y
 cv2.rectangle(image, (left_high[0], left_high[1]), (right_low[0], right_low[1]), (255, 0, 0), 10)
-cv2.imwrite('output.jpg', mask)
-cv2.waitKey(0)
+cv2.imwrite('output.jpg', image)
