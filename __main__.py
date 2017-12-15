dd0171eca60c5ed9a087edeb248377f53383a118
N. Keijzer <n.keijzer@student.utwente.nl>
2017-12-15 12:29:57 +0100

    Update controller

--- __main__.py
+++ __main__.py
@@ -1,25 +1,33 @@
 import threading
-import controller
+import updatedController
 import imageProcessor
 import Queue
+import time
+import sys
 
 
 def main():
-    controller_queue = Queue.Queue()
+    stop = threading.Event()
+    try:
+        controller_queue = Queue.Queue()
 
-    # define the two threads
-    image_thread = threading.Thread(target=imageProcessor.main, args=controller_queue)
-    controller_thread = threading.Thread(target=controller.main, args=controller_queue)
+        # define the two threads
+        image_thread = threading.Thread(target=imageProcessor.main, args=(controller_queue,stop))
+        controller_thread = threading.Thread(target=updatedController.main, args=(controller_queue,stop))
 
-    # start the two threads
-    image_thread.start()
-    controller_thread.start()
+        # start the two threads
+        image_thread.start()
+        controller_thread.start()
 
-    # wait for the two threads to finish
-    image_thread.join()
-    controller_thread.join()
+        # wait for the two threads to finish
+        while image_thread.is_alive() or controller_thread.is_alive():
+            time.sleep(0.5)
 
-    print "[Thread main\t: stopping"
+        print "[Thread main\t: stopping"
+    except (KeyboardInterrupt, SystemExit):
+        print "exception occurred"
+        stop.set()
+        sys.exit(0)
 
 
 if __name__ == '__main__':
--- updatedController.py
+++ updatedController.py
@@ -1,14 +1,14 @@
-# 1/usr/bin/python
+# /usr/bin/python
 from gopigo import *
 from imageRecognizer import *
 import time
 import Queue
 import sys
 import signal
+import threading
 
-DEFAULT_LEFT_SPEED = 255
-DEFAULT_RIGHT_SPEED = 200
-
+DEFAULT_LEFT_SPEED = 127
+DEFAULT_RIGHT_SPEED = 100
 
 def do180():
     '''
@@ -16,34 +16,62 @@ def do180():
     '''
     right_rot()
     time.sleep(0.5)
+    print "did 180"
     stop()
+    print "stopped after 180"
 
 
-def main(queue):
+def find_bottle(queue):
+    time.sleep(0.25)
+    while queue.empty():
+        right_rot()
+        time.sleep(0.15)
+        stop()
+        time.sleep(0.25)
+
+
+def main(queue,stop_event):
     left_speed = DEFAULT_LEFT_SPEED
     right_speed = DEFAULT_RIGHT_SPEED
     do180()
+    print "after 180 main"
+    find_bottle(queue)
+    print "Bottle has been found"
     set_left_speed(left_speed)
     set_right_speed(right_speed)
+    print "set speeds"
     fwd()
+    print "called forward"
     j = 0
-    while j < 500:
+    previous_command = 2
+    while j < 500 and not stop_event.is_set():
         try:
-            location = queue.get()
-            if location == 1:
-                right_speed -= right_speed / 20
-                set_right_speed(right_speed)
-            elif location == -1:
-                left_speed -= left_speed / 20
-                set_left_speed(left_speed)
-            elif location == 0:
-                stop()
-                break
+            if not queue.empty():
+                location = queue.get()
+                if location == 1:
+                    if previous_command == 1:
+                        right_speed -= right_speed / 5
+                    else:
+                        right_speed -= right_speed / 10
+                    set_right_speed(right_speed)
+                    previous_command = 1
+                elif location == -1:
+                    if previous_command == -1:
+                        left_speed -= left_speed / 5
+                    else:
+                        left_speed -= left_speed / 10
+                    set_left_speed(left_speed)
+                    previous_command = -1
+                elif location == 0:
+                    stop()
+                    break
             else:
-                left_speed = DEFAULT_LEFT_SPEED
-                right_speed = DEFAULT_RIGHT_SPEED
-                set_left_speed(left_speed)
-                set_right_speed(right_speed)
+                if previous_command = 2:
+                    left_speed = DEFAULT_LEFT_SPEED
+                    right_speed = DEFAULT_RIGHT_SPEED
+                    set_left_speed(left_speed)
+                    set_right_speed(right_speed)
+                previous_command = 2
             j += 1
         except:
             print "exception occurred, terminating program."
@@ -53,12 +81,6 @@ def main(queue):
     stop()
 
 
-def signal_handler(signal, frame):
-    print "signal handler called, terminating program."
-    stop()
-    sys.exit(0)
-
-
 if __name__ == '__main__':
     q = Queue.Queue()
-    main(q)
+    main(q,threading.Event())
